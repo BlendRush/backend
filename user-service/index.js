ed13e83f685166376312dfe0b365b1b2abc3e46a
ImashaNawodi <imashanaw1999@gmail.com>
2025-08-08 13:25:10 +0530

    user service completed

---
+++ user-service/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./src/routes/userRoutes');
+require('dotenv').config();
+
+const app = express();
+const PORT = 3003;
+
+app.use(express.json());
+app.use('/user', userRoutes);
+
+// MongoDB Atlas connection
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.listen(PORT, () => {
+  console.log(`User service running on port ${PORT}`);
+});
