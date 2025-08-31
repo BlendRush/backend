b14ca1a68795f24d02f3d4de198173f4b13be3c2
ImashaNawodi <imashanaw1999@gmail.com>
2025-08-31 22:24:18 +0530

    update index.js

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require("cors");
 const app = express();
 
-app.use('/api/menu', createProxyMiddleware({ target: 'http://menu-service:3001', changeOrigin: true }));
+/* app.use('/api/menu', createProxyMiddleware({ target: 'http://menu-service:3001', changeOrigin: true }));
 app.use('/api/orders', createProxyMiddleware({ target: 'http://order-service:3002', changeOrigin: true }));
-app.use('/api/user', createProxyMiddleware({ target: 'http://user-service:3003', changeOrigin: true }));
+app.use('/api/user', createProxyMiddleware({ target: 'http://user-service:3003', changeOrigin: true })); */
+
+app.use(cors({
+    origin: 'http://localhost:3001',
+    methods: ['GET','POST','PUT','DELETE'],
+    credentials: true
+}));
+
+app.use('/api/menu', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/api/orders', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/api/user', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
 
 app.listen(3000, () => {
     console.log("API Gateway running on port 3000");
