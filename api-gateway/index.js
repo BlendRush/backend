a4457c5406156a5a63ed857ce37273faf6e2e401
ImashaNawodi <imashanaw1999@gmail.com>
2025-09-04 13:53:04 +0530

    add cart service

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -16,6 +16,7 @@ app.use(cors({
 app.use('/api/menu', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 app.use('/api/orders', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
 app.use('/api/user', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/api/carts', createProxyMiddleware({ target: 'http://localhost:3005', changeOrigin: true }));
 
 app.listen(3000, () => {
     console.log("API Gateway running on port 3000");
