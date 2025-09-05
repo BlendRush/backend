560cc57e1db1f50b4b27e194fb374cd41d56ae7f
supunijayasinghe <supuninavodya695@gmail.com>
2025-09-05 17:28:36 +0530

    update the index file

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -67,4 +67,3 @@ app.use('/api/user', createProxyMiddleware({ target: 'http://user-service:3003',
 app.use('/api/carts', createProxyMiddleware({ target: 'http://cart-service:3005', changeOrigin: true }));
 
 app.listen(3000, () => console.log("API Gateway running on port 3000"));
-app.listen(3000, () => console.log("API Gateway running on port 3000"));
